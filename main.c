4223b8af86549c7c67ca527589451ba5b6a082da
Fadwa Aljaoui <faljaoui@e1r13p5.1337.ma>
2022-05-26 08:50:03 +0100

    try some stuff in bitsift and rgb <3

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include "so_long.h"
 #include <math.h>
 // #include <mlx.h>
+#include<stdio.h>
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
 int paint(void *mlx, void *win, int X, int Y, int color)
 {
     int i =0;
@@ -15,6 +20,7 @@ while(j<Y)
     }
     i=0;
     j++;
+    
 }
 return(0);
 }
@@ -22,8 +28,8 @@ int main()
 {
     void *mlx = mlx_init();
     void *win = mlx_new_window(mlx, 1920 , 1080 , "PAINT MY BOARD WINDOW IN PINK HEHE <3");
-    paint(mlx, win, 1920,1080, 0xFFB6C1);
-	 mlx_string_put ( mlx,win,900, 1080/2, 0xE75480 ,"good job <3 ");
+    paint(mlx, win, 1920,1080, create_trgb(100,0,45,255));
+	 mlx_string_put ( mlx,win,900, 1080/2, 800000 ,"good job <3 ");
 
     mlx_loop(mlx);
 

c69eb2aceef0ce16c66916921b574acd4f2000a3
Fadwa Aljaoui <faljaoui@e1r9p12.1337.ma>
2022-05-27 02:10:50 +0100

    PAINT MYunicornin xpm HEHE <3

--- main.c
+++ main.c
@@ -2,34 +2,43 @@
 #include <math.h>
 // #include <mlx.h>
 #include<stdio.h>
-int	create_trgb(int t, int r, int g, int b)
-{
-	return (t << 24 | r << 16 | g << 8 | b);
-}
-int paint(void *mlx, void *win, int X, int Y, int color)
-{
-    int i =0;
-    int j=0;
-while(j<Y)
-{
-    mlx_pixel_put(mlx,win,i,j,color);
-	while(i<X )
-    {
-        mlx_pixel_put(mlx,win,i,j,color);
-        i++;
-    }
-    i=0;
-    j++;
-    
-}
-return(0);
-}
+// int	create_trgb(int t, int r, int g, int b)
+// {
+// 	return (t << 24 | r << 16 | g << 8 | b);
+// }
+// int paint(void *mlx, void *win, int X, int Y, int color)
+// {
+//     int i =0;
+//     int j=0;
+// while(j<Y)
+// {
+//     mlx_pixel_put(mlx,win,i,j,color);
+// 	while(i<X )
+//     {
+//         mlx_pixel_put(mlx,win,i,j,color);
+//         i++;
+//     }
+//     i=0;
+//     j++;
+// }
+// return(0);
+// }
 int main()
 {
+	void	*img;
+		int		img_width;
+	int		img_height;
+	img_height=100;
+	img_width=50;
+	char	*relative_path = "./unicorn.xpm";
     void *mlx = mlx_init();
-    void *win = mlx_new_window(mlx, 1920 , 1080 , "PAINT MY BOARD WINDOW IN PINK HEHE <3");
-    paint(mlx, win, 1920,1080, create_trgb(100,0,45,255));
-	 mlx_string_put ( mlx,win,900, 1080/2, 800000 ,"good job <3 ");
+   void *win = mlx_new_window(mlx, 1920 , 1080 , "PAINT MYunicornin xpm HEHE <3");
+    // paint(mlx, win, 1920,1080, create_trgb(100,0,45,255));
+//	 mlx_string_put ( mlx,win,900, 1080/2, 800000 ,"good job <3 ");
+
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+              mlx_put_image_to_window(mlx, win, img,800, 500);
+
 
     mlx_loop(mlx);
 
--- so_long.h
+++ so_long.h
@@ -7,6 +7,43 @@
 # include <mlx.h>
 # include <stdio.h>
 
+typedef struct	s_vector
+{
+	int	x;
+	int	y;
+}				t_vector;
+
+/* A pointer to the window and its size */
+typedef struct	s_window {
+	void		*reference;
+	t_vector	size;
+}				t_window;
+
+/* The 4 values that define a color */
+typedef struct s_color {
+	int	r;
+	int g;
+	int b;
+	int a;
+}	t_color;
+
+/* all info needed for an image */
+typedef struct	s_image {
+	void		*reference;
+	t_vector	size;
+	char		*pixels;
+	int			bits_per_pixel;
+	int			line_size;
+	int			endian;
+}				t_image;
+
+typedef struct	s_program {
+	void		*mlx;
+	t_window	window;
+	t_image		sprite;
+	t_vector	sprite_position;
+}				t_program;
+
 char			f_j(char c);
 
 #endif
